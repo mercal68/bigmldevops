2172af09a64db3d18189ffba8fcb7c714bffa7a3
mercal68 <mercadoal68@gmail.com>
2019-08-20 16:18:12 -0400

    Add files via upload

---
+++ bquery_func.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+from google.cloud import bigquery
+
+def my_query(request):
+    df_query = pd.read_gbq(request,project_id='wipro-idp-env',dialect='standard')
+    return df_query
+  
+  
+  
+if __name__ == "__main__":
+    my_request = """ select * from aramark_poc.PAM_with_tier_price limit 100 """
+    print (len(my_query(my_request)))
+    
